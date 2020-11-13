5ab074c4c25e2af28a773476f199e7b25f4d9b32
Nikita Stupin <18281368+nikitastupin@users.noreply.github.com>
2020-11-13 15:26:03 +0300

    Replace swapi with own GraphQL API

--- tests/system.py
+++ tests/system.py
@@ -1,24 +1,25 @@
 import os
 import json
+import time
 import unittest
 import subprocess
-import time
+
+from typing import Any
+from typing import Dict
 
 
 class TestClairvoyance(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.cwd = "/tmp/swapi-graphql"
-        cls.port = "1337"
-
-        env = os.environ
-        env["PORT"] = cls.port
-
-        cls.swapi = subprocess.Popen(["npm", "start"], env=env, cwd=cls.cwd)
-        time.sleep(5)
-
+        cls.port = "4000"
         output_file = "/tmp/t.json"
-        cls.p = subprocess.run(
+
+        try:
+            os.remove(output_file)
+        except FileNotFoundError:
+            pass
+
+        cls.clairvoyance = subprocess.run(
             [
                 "python3",
                 "-m",
@@ -26,38 +27,129 @@ class TestClairvoyance(unittest.TestCase):
                 "-o",
                 output_file,
                 "-w",
-                "tests/data/swapi/wordlist.txt",
+                "tests/data/wordlist-for-apollo-server.txt",
                 f"http://localhost:{cls.port}",
             ],
             capture_output=True,
         )
 
-        cls.swapi.terminate()
-        cls.swapi.wait()
-
         with open(output_file) as f:
-            cls.schema = json.load(f)
+            j = json.load(f)
 
-        cls._schema = cls.schema["data"]["__schema"]
+        cls.schema = j["data"]["__schema"]
+
+    @property
+    def query_type(self):
+        query_type = self.get_type(self.schema["queryType"]["name"])
+
+        if not query_type:
+            raise Exception(f"Schema don't contain query type")
+
+        return query_type
+
+    def get_type(self, name: str) -> Dict[str, Any]:
+        for t in self.schema["types"]:
+            if t["name"] == name:
+                return t
+
+        return None
 
     @unittest.skip("Clairvoyance produces false positive warnings")
     def test_no_warnings(self):
         self.assertFalse(p.stderr)
 
-    def test_found_valid_root_types(self):
-        self.assertEqual(self._schema["queryType"], {"name": "Root"})
-        self.assertIsNone(self._schema["mutationType"])
-        self.assertIsNone(self._schema["subscriptionType"])
+    def test_found_root_type_names(self):
+        self.assertEqual(self.schema["queryType"], {"name": "Query"})
+        self.assertEqual(self.schema["mutationType"], {"name": "Mutation"})
+        self.assertIsNone(self.schema["subscriptionType"])
 
-    def test_found_valid_field(self):
-        self.assertIn("Vehicle", [t["name"] for t in self._schema["types"]])
+    def test_type_names(self):
+        type_names = [t["name"] for t in self.schema["types"]]
+
+        self.assertIn("Query", type_names)
+        self.assertIn("Mutation", type_names)
+        self.assertIn("Launch", type_names)
+        self.assertIn("Rocket", type_names)
+        self.assertIn("User", type_names)
+        self.assertIn("Mission", type_names)
+        self.assertIn("TripUpdateResponse", type_names)
+
+    def test_query_basics(self):
+        query_type = self.query_type
+
+        self.assertEqual(query_type["name"], "Query")
+        self.assertEqual(query_type["kind"], "OBJECT")
+
+    def test_query_field_names(self):
+        query_fields = self.query_type["fields"]
+        field_names = [f["name"] for f in query_fields]
+
+        self.assertEqual(len(field_names), 3)
+        self.assertIn("launches", field_names)
+        self.assertIn("launch", field_names)
+        self.assertIn("me", field_names)
+
+    def test_query_field_types(self):
+        query_fields = self.query_type["fields"]
+
+        for f in query_fields:
+            if f["name"] == "launches":
+                want = {
+                    "kind": "NON_NULL",
+                    "name": None,
+                    "ofType": {
+                        "kind": "LIST",
+                        "name": None,
+                        "ofType": {
+                            "kind": "OBJECT",
+                            "name": "Launch",
+                            "ofType": None,
+                        },
+                    },
+                }
+                self.assertEqual(f["type"], want)
+            elif f["name"] == "launch":
+                want = {"kind": "OBJECT", "name": "Launch", "ofType": None}
+                self.assertEqual(f["type"], want)
+            elif f["name"] == "me":
+                want = {"kind": "OBJECT", "name": "User", "ofType": None}
+                self.assertEqual(f["type"], want)
+            else:
+                self.fail(f"Unexpected field {f['name']} on query type")
+
+    def test_scalar_arguments(self):
+        query_fields = self.query_type["fields"]
+
+        for f in query_fields:
+            if f["name"] == "launch":
+                self.assertEqual(len(f["args"]), 1)
+                self.assertEqual(f["args"][0]["name"], "id")
+
+                want = {
+                    "kind": "NON_NULL",
+                    "name": None,
+                    "ofType": {"kind": "SCALAR", "name": "ID", "ofType": None},
+                }
+
+                self.assertEqual(f["args"][0]["type"], want)
+
+    def test_nonroot_type_field_names(self):
+        nonroot_type = self.get_type("User")
+        field_names = [f["name"] for f in nonroot_type["fields"]]
+
+        self.assertEqual(len(field_names), 3)
+        self.assertIn("id", field_names)
+        self.assertIn("email", field_names)
+        self.assertIn("trips", field_names)
 
     @classmethod
     def tearDownClass(cls):
-        if cls.p.stdout:
-            print(cls.p.stdout)
-        if cls.p.stderr:
-            print(cls.p.stderr)
+        if cls.clairvoyance.stdout:
+            with open("/tmp/clairvoyance-tests.stdout", "wb") as f:
+                f.write(cls.clairvoyance.stdout)
+        if cls.clairvoyance.stderr:
+            with open("/tmp/clairvoyance-tests.stderr", "wb") as f:
+                f.write(cls.clairvoyance.stderr)
 
 
 if __name__ == "__main__":
