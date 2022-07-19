bbae28b1898264285955e604f8905aa71015a30b
c3b5aw <dev@c3b5aw.dev>
2022-07-19 12:01:38 +0200

    feat: config

---
+++ clairvoyance/config.py
@@ -0,0 +1,31 @@
+import logging
+
+from typing import Dict, Optional
+from clairvoyance.client import Client
+
+
+class Config:
+
+    def __init__(
+        self,
+        url: str,
+        headers: Optional[Dict[str, str]] = None,
+        logger: Optional[logging.Logger] = None,
+    ) -> None:
+        self._url: str = url
+        self._headers: dict[str, str] = headers or {}
+        self._logger = logger or logging.getLogger('clairvoyance')
+        self._bucket_size: int = 512
+
+        self._client = Client(
+            self._url,
+            self._headers,
+        )
+
+    @property
+    def client(self) -> Client:
+        return self._client
+
+    @property
+    def logger(self) -> logging.Logger:
+        return self._logger
